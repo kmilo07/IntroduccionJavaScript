79aa0a37ff6878f7c92b357c5835a9c3cf5c27d8
kmilo07 <camilo.gil2707@hotmail.com>
2021-05-11 17:41:37 +0000

    Se agregaron las imagenes a carpeta y se creo otras funcionalidades

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function myFunction() {
   document.getElementById("demo").innerHTML = "Paragraph changed.";
 }
+
+
+function poweronlightbulb(){
+  document.getElementById('myImage').src='https://cdn.jsdelivr.net/gh/kmilo07/IntroduccionJavaScript@master/Screenshot%20from%202021-05-11%2016-49-31.png';
+}
+function powerofflightbulb(){
+  document.getElementById('myImage').src='https://cdn.jsdelivr.net/gh/kmilo07/IntroduccionJavaScript@master/Screenshot%20from%202021-05-11%2016-48-00.png';
+}
